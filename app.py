729fc013b3013e3d338d33a027a4d30cbf71e64d
root <root@ip-10-0-1-60.ec2.internal>
2021-03-20 05:36:08 +0000

    update code

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, request, render_template, redirect, session, url_for
+import os
+from dotenv import load_dotenv
+
+#Load property file
+dotenv_path = 'application.properties'  
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
+#app.config.from_envvar('MYECHO_CONFIG')
 
 @app.route('/', methods=['GET', 'POST'])
 def echo():
     if request.method == 'POST' and 'msg' in request.form:
         data = request.form.get('msg')
-        return render_template('echo.html',data=data)
+        return render_template('echo.html',data=data,appenv=os.environ.get("FLASK_ENV"))
     else:
         data = ''
-        return render_template('echo.html',data=data)
+        return render_template('echo.html',data=data,appenv=os.environ.get("FLASK_ENV"))
     data = ''
     return render_template('echo.html')
 
